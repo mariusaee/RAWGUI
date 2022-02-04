a5a1294deab7ad9be75892f5f44311cca6d03a71
mariusaee <mariusae@me.com>
2022-02-04 15:35:27 +0300

    gameCard

---
+++ RAWGUI/Models/Game.swift
@@ -0,0 +1,12 @@
+//
+//  Game.swift
+//  RAWGUI
+//
+//  Created by Marius Malyshev on 03.02.2022.
+//
+
+struct Game: Codable {
+    let name: String?
+    let description_raw: String?
+    let background_image: String?
+}
---
+++ RAWGUI/ViewControllers/GameViewController.swift
@@ -0,0 +1,52 @@
+//
+//  GameViewController.swift
+//  RAWGUI
+//
+//  Created by Marius Malyshev on 03.02.2022.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+    
+    @IBOutlet var backgroundImage: UIImageView!
+    @IBOutlet var gameNameLabel: UILabel!
+    @IBOutlet var aboutGameTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        fetchGame()
+    }
+    
+    private func fetchGame() {
+        let urlString = "https://api.rawg.io/api/games/22511?key="
+        let apiKey = "e29e1df3581e4b07b4b7ea370b4cda67"
+        
+        guard let url = URL(string: urlString + apiKey) else { return }
+        print(url)
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data else {
+                print(error?.localizedDescription ?? "No description")
+                return
+            }
+            do {
+                let game = try JSONDecoder().decode(Game.self, from: data)
+                
+                DispatchQueue.main.async {
+                    guard let imageString = game.background_image else { return }
+                    guard let imageUrl = URL(string: imageString) else { return }
+                    guard let imageData = try? Data(contentsOf: imageUrl) else { return }
+                    self.backgroundImage.image = UIImage(data: imageData)
+                    self.gameNameLabel.text = game.name
+                    self.aboutGameTextView.text = game.description_raw
+                }
+            } catch let jsonError {
+                print(jsonError)
+            }
+            
+        }.resume()
+        
+    }
+}
+
