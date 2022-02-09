fc1f4e9caa0b730f3a41352939c35084c68f60ee
mariusaee <mariusae@me.com>
2022-02-09 13:10:35 +0300

    TableViewCell

--- RAWGUI/ViewControllers/GamesTableViewController.swift
+++ RAWGUI/ViewControllers/GamesTableViewController.swift
@@ -9,66 +9,40 @@ import UIKit
 
 class GamesTableViewController: UITableViewController {
     
-    private var games: [Game] = []
-
+    //MARK: Private properties
+    private var games: [Game]?
+   
+    // MARK: - UIViewController Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.rowHeight = 70
         fetchGames(from: Link.randomGames.rawValue)
     }
 
     // MARK: - Table view data source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        games.count
+        games?.count ?? 0
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        
         var content = cell.defaultContentConfiguration()
-        content.text = games[indexPath.row].name
-        content.image = UIImage(data: ImageManager.shared.fetchImage(from: games[indexPath.row].backgroundImage)!)
-        content.imageProperties.maximumSize = CGSize(width: 50, height: 50)
-        cell.contentConfiguration = content
-        
+
+        content.text = games?[indexPath.row].name
+
+        DispatchQueue.global().async {
+            guard let stringUrl = self.games?[indexPath.row].backgroundImage else { return }
+            guard let imageUrl = URL(string: stringUrl) else { return }
+            guard let imageData = try? Data(contentsOf: imageUrl) else { return }
+            DispatchQueue.main.async {
+                content.image = UIImage(data: imageData)
+                content.imageProperties.maximumSize = CGSize(width: 50, height: 50)
+                cell.contentConfiguration = content
+            }
+        }
         return cell
     }
-    
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 
     /*
     // MARK: - Navigation
