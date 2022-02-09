fb5dcc03794de58213dbe13e0f98b05def58398b
mariusaee <mariusae@me.com>
2022-02-09 14:37:48 +0300

    configure custom item

--- RAWGUI/Views/GameCollectionViewCell.swift
+++ RAWGUI/Views/GameCollectionViewCell.swift
@@ -9,4 +9,11 @@ import UIKit
 
 class GameCollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet var gameImage: UIImageView!
+    @IBOutlet var gameNameLabel: UILabel!
+    
+    func configureItem(with game: Game) {
+        gameNameLabel.text = game.name
+        
+    }
 }
