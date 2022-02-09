7b82da5e14d6a2fd5bdcb88bf6b9d743fffb789e
mariusaee <mariusae@me.com>
2022-02-09 15:17:13 +0300

    GameCollectionViewCell

--- RAWGUI/Views/GameCollectionViewCell.swift
+++ RAWGUI/Views/GameCollectionViewCell.swift
@@ -15,5 +15,15 @@ class GameCollectionViewCell: UICollectionViewCell {
     func configureItem(with game: Game) {
         gameNameLabel.text = game.name
         
+        DispatchQueue.global().async {
+            guard let imageString = game.backgroundImage else { return }
+            guard let imageURL = URL(string: imageString) else { return }
+            guard let imageData = try? Data(contentsOf: imageURL) else { return }
+            
+            DispatchQueue.main.async {
+                self.gameImage.layer.cornerRadius = 15
+                self.gameImage.image = UIImage(data: imageData)
+            }
+        }
     }
 }
