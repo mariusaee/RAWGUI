0cbeb60a3e089ac0db46391d858007700176b7e1
Marius <mariusae@me.com>
2022-04-11 18:28:03 +0300

    navigation

--- RAWGUI/ViewControllers/GamesTableViewController.swift
+++ RAWGUI/ViewControllers/GamesTableViewController.swift
@@ -44,41 +44,6 @@ class GamesTableViewController: UITableViewController {
         }
     }
     
-    /*
-     // Override to support conditional editing of the table view.
-     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the specified item to be editable.
-     return true
-     }
-     */
-    
-    /*
-     // Override to support editing the table view.
-     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-     if editingStyle == .delete {
-     // Delete the row from the data source
-     tableView.deleteRows(at: [indexPath], with: .fade)
-     } else if editingStyle == .insert {
-     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-     }
-     }
-     */
-    
-    /*
-     // Override to support rearranging the table view.
-     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-     
-     }
-     */
-    
-    /*
-     // Override to support conditional rearranging of the table view.
-     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the item to be re-orderable.
-     return true
-     }
-     */
-    
     // MARK: - Private methods
     private func setupSearchController() {
         navigationItem.searchController = searchController
@@ -104,14 +69,15 @@ class GamesTableViewController: UITableViewController {
     }
     
     // MARK: - Navigation
-    /*
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let gameVC = segue.destination as? GameViewController else { return }
+        
+        guard let indexPaths = tableView.indexPathsForSelectedRows else { return }
+        guard let selectedGameIndex = indexPaths.first?.item else { return }
+        let game = games[selectedGameIndex]
+        
+        gameVC.game = game
+    }
 }
 
 // MARK: - UISearchBarDelegate
@@ -119,9 +85,9 @@ extension GamesTableViewController: UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         games = []
         timer?.invalidate()
-        timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false, block: { _ in
+        timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false) { _ in
             self.fetchGames(from: Link.search.rawValue + searchText.replacingOccurrences(of: " ", with: "+"))
-        })
+        }
     }
     
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
